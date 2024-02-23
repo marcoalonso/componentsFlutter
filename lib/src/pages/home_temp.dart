ed536b7c21efb631773d53ea0fd5e61cff755a94
Marco Alonso Rodriguez <mrodriguez@epica.solutions>
2024-02-23 10:53:57 -0600

    Simple String list added

---
+++ lib/src/pages/home_temp.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class HomePageTemp extends StatelessWidget {
+
+  final opciones = ["Swift", "JAVA", "Python", "Kotlin", "Dart", "C#"];
+  
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Componentes Temp"),
+      ),
+      body: ListView(
+        children: _crearItems()
+      ),
+    );
+  }
+
+  List<Widget> _crearItems() {
+
+    List<Widget> lista = [];
+
+    for (String opt in opciones) {
+      
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+
+      lista..add(tempWidget)
+            ..add(Divider());
+
+    }
+    return lista;
+  }
+
+} 
